42ba5e80be75077bcc0ee967f68e14f942af8704
Pranjala Shukla <48888238+pranjalashukla@users.noreply.github.com>
2020-10-02 22:08:22 +0530

    Update Test.py
    Updating variable name

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
-li= [(1,2),3,[4,5],6]
-for i in li:
+sample_data = [(1,2),3,[4,5],6]
+for i in sample_data:
     if (type ( i ) is list )or (type ( i ) is tuple):
         for j in i:
             print(j,end=" ")
